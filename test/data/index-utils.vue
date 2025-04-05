3b663966a585ecffc08ec3a2d0d49e4c92010a77
DBAAZzz <1292094030@qq.com>
2025-04-05 15:40:06 +0800

    feat: 完善单元测试

---
+++ test/data/index-utils.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>我是默认的界面</div>
+  <button id="button" @click="disableMpBack"></button>
+  <button id="button2" @click="activeMpBack"></button>
+</template>
+
+<script setup>
+import onPageBack, { activeMpBack as mpppacitve, inactiveMpBack } from 'mp-weixin-back-helper'
+
+onPageBack(
+  () => {
+    console.log('触发了手势返回')
+  },
+  {
+    initialValue: false,
+  }
+)
+
+const activeMpBack = () => {
+  console.log('执行了activeMpBack')
+  mpppacitve()
+}
+
+const disableMpBack = () => {
+  inactiveMpBack()
+}
+</script>
+
+<style></style>
